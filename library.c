57558088bd7eda03d20139c407fb4cf26b35b511
Eric Hunzeker <emh128.pitt.edu>
2018-09-11 16:42:48 -0400

    add c files and header

--- library.c
+++ library.c
@@ -21,7 +21,7 @@ void clear_screen(void *img) {}
 
 void draw_pixel(void *img, int x, int y, color_t color) {}
 
-void draw_line(void *img, int x1, int y1, int x2, int y2, colot_t c) {}
+void draw_line(void *img, int x1, int y1, int x2, int y2, color_t c) {}
 
 void *new_offscreen_buffer() {}
 
