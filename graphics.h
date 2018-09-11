40f03b12b5d9e9e194b67e45fafc351f0ff92b19
Eric Hunzeker <emh128.pitt.edu>
2018-09-11 16:41:33 -0400

    add c files and header

---
+++ graphics.h
@@ -0,0 +1,19 @@
+//graphics.h - project 1 - emh128
+#ifndef GRAPHICS
+#define GRPAHICS
+
+typedef unsigned short int color_t;
+
+#define RGB(R, G, B) (((R & 0x1F) << 11) | ((G & 0x3F) << 5) | (B &0x1F));
+
+void init_graphics();
+void exit_graphics();
+char getkey();
+void sleep_ms(long ms);
+void clear_screen(void *img);
+void draw_pixel(void *img, int x, int y, color_t color);
+void draw_line(void *img, int x1, int y1, int x2, int y2, color_t c);
+void* new_offscreen_buffer();
+void blit(void *src);
+
+#endif
---
+++ library.c
@@ -0,0 +1,29 @@
+#include "graphics.h"
+#include "sys/stat.h"
+#include <fcntl.h>
+#include <sys/mman.h>
+
+
+static int framebuffer;
+
+void init_graphics() {
+	framebuffer = open("/dev/fb0", O_RDWR);
+	
+}
+
+void exit_graphics() {}
+
+char getkey() {}
+
+void sleep_ms(long ms) {}
+
+void clear_screen(void *img) {}
+
+void draw_pixel(void *img, int x, int y, color_t color) {}
+
+void draw_line(void *img, int x1, int y1, int x2, int y2, colot_t c) {}
+
+void *new_offscreen_buffer() {}
+
+void blit(void *src) {}
+
