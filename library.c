4f5a370ccf4823934bcb3a6e85f637256cafbc14
Eric Hunzeker <emh128.pitt.edu>
2018-09-13 14:42:03 -0400

    need to do draw_line in library.c and then snake.c

--- library.c
+++ library.c
@@ -14,14 +14,14 @@ int framebuffer;
 void* fb_pointer;
 int yres;
 int linelength;
-int totalmem;
+int memsize;
 
 void init_graphics() {
 
     //size of mmap()’ed file = (yres_virtual field of the first struct) * (line_length field of the second)
 
     struct fb_var_screeninfo varScreeninfo;
-    struct fb_fb_fix_screeninfo fixScreeninfo;
+    struct fb_fix_screeninfo fixScreeninfo;
     struct termios termios;
 
     framebuffer = open("/dev/fb0", O_RDWR);
@@ -31,9 +31,9 @@ void init_graphics() {
 
     yres = varScreeninfo.yres_virtual;
     linelength = fixScreeninfo.line_length;
-    totalmem = yres * linelength;
+    memsize = yres * linelength;
 
-    fb_pointer = mmap(NULL, totalmem, PROT_WRITE, MAP_SHARED, framebuffer, 0);
+    fb_pointer = mmap(NULL, memsize, PROT_WRITE, MAP_SHARED, framebuffer, 0);
 
     ioctl(1, TCGETS, &termios);
     termios.c_lflag &= ~ECHO;
@@ -45,7 +45,7 @@ void init_graphics() {
 void exit_graphics() {
     struct termios termios;
 
-    munlock(fb_pointer, totalmem);
+    munmap(fb_pointer, memsize);
     ioctl(1, TCGETS, &termios);
     termios.c_lflag |= ECHO;
     termios.c_lflag |= ICANON;
@@ -74,13 +74,26 @@ void clear_screen(void *img) {
     write(1, "\033[2J", 8);
 }
 
-void draw_pixel(void *img, int x, int y, color_t color) {}
-
-void draw_line(void *img, int x1, int y1, int x2, int y2, color_t c) {}
-
-void *new_offscreen_buffer() {}
-
-void blit(void *src) {
-
+void draw_pixel(void *img, int x, int y, color_t color) {
+    color_t* pixel = (framebuffer + (y * linelength + x));
+    *pixel = color;
 }
 
+void draw_line(void *img, int x1, int y1, int x2, int y2, color_t c) {
+    //draw mfkn line
+}
+
+void* new_offscreen_buffer() {
+    void* buff = mmap(NULL, memsize, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    return buff;
+}
+
+void blit(void *src) {
+    //copy every byte from *src buffer to framebuffer
+    int i;
+    for (i = 0; i < memsize; i++) {
+        *(i + (char *) (framebuffer)) = *(i + (char *) (src)); //get single byte in form of char
+    }
+
+
+}
