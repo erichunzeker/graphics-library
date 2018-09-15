4d16602c09daa0f6bfcfa406c8ac158cccd33de5
Eric Hunzeker <emh128.pitt.edu>
2018-09-15 18:32:20 -0400

    finished part 1 i think

--- library.c
+++ library.c
@@ -8,7 +8,7 @@
 #include <time.h>
 #include <linux/fb.h>
 #include <termios.h>
-
+#include <stdlib.h>
 
 int framebuffer;
 void* fb_pointer;
@@ -17,9 +17,7 @@ int linelength;
 int memsize;
 
 void init_graphics() {
-
     //size of mmap()’ed file = (yres_virtual field of the first struct) * (line_length field of the second)
-
     struct fb_var_screeninfo varScreeninfo;
     struct fb_fix_screeninfo fixScreeninfo;
     struct termios termios;
@@ -30,27 +28,28 @@ void init_graphics() {
 	ioctl(framebuffer, FBIOGET_FSCREENINFO, &fixScreeninfo);
 
     yres = varScreeninfo.yres_virtual;
-    linelength = fixScreeninfo.line_length;
-    memsize = yres * linelength;
+    linelength = fixScreeninfo.line_length / 2;
+    memsize = yres * linelength * 2;
 
-    fb_pointer = mmap(NULL, memsize, PROT_WRITE, MAP_SHARED, framebuffer, 0);
+    fb_pointer = mmap(NULL, memsize, PROT_READ | PROT_WRITE, MAP_SHARED, framebuffer, 0);
 
-    ioctl(1, TCGETS, &termios);
+    ioctl(framebuffer, TCGETS, &termios);
     termios.c_lflag &= ~ECHO;
     termios.c_lflag &= ~ICANON;
-    ioctl(1, TCSETS, &termios);
+    ioctl(framebuffer, TCSETS, &termios);
 
 }
 
 void exit_graphics() {
-    struct termios termios;
+    struct termios t;
 
     munmap(fb_pointer, memsize);
-    ioctl(1, TCGETS, &termios);
-    termios.c_lflag |= ECHO;
-    termios.c_lflag |= ICANON;
-    ioctl(1, TCSETS, &termios);
     close(framebuffer);
+
+    ioctl(framebuffer, TCGETS, &t);
+    t.c_lflag |= ECHO;
+    t.c_lflag |= ICANON;
+    ioctl(framebuffer, TCSETS, &t);
 }
 
 char getkey() {
@@ -71,17 +70,34 @@ void sleep_ms(long ms) {
 }
 
 void clear_screen(void *img) {
-    write(1, "\033[2J", 8);
+    write(STDOUT_FILENO, "\033[2J", 4);
 }
 
 void draw_pixel(void *img, int x, int y, color_t color) {
-
+    *((color_t*)(img) + (y * linelength + x)) = color;
 }
 
 void draw_line(void *img, int x1, int y1, int x2, int y2, color_t c) {
+    //https://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C
 
-    draw_pixel(img, x1, y1, c);
-    draw_pixel(img, x2, y2, c);
+    int dx = abs(x2-x1), sx = x1<x2 ? 1 : -1;
+    int dy = abs(y2-y1), sy = y1<y2 ? 1 : -1;
+    int err = (dx>dy ? dx : -dy)/2, e2;
+
+    for(;;){
+        draw_pixel(img, x1,y1, c);
+        if (x1==x2 && y1==y2)
+            break;
+        e2 = err;
+        if (e2 >-dx) {
+            err -= dy;
+            x1 += sx;
+        }
+        if (e2 < dy) {
+            err += dx;
+            y1 += sy;
+        }
+    }
 }
 
 void* new_offscreen_buffer() {
@@ -95,6 +111,4 @@ void blit(void *src) {
     for (i = 0; i < memsize; i++) {
         *(i + (char *) (fb_pointer)) = *(i + (char *) (src)); //get single byte in form of char
     }
-
-
 }
